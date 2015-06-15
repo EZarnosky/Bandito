6209bf4db6540b56bf97a539b7d47714552c9a71
ezarnosky <ezarnosky@gmail.com>
2015-06-15 10:40:19 -0700

    initial install script, will need tweaks

--- scripts/add-rproxy-nginx.sh
+++ scripts/add-rproxy-nginx.sh
@@ -1,1 +1,6 @@
+#---> Install Nginx
+sudo apt-get install nginx -y
 
+# TODO - Setup soft links to bandito conf directory for nginx conf files and bandito logs directory for nginx logs
+
+sudo service nginx start
