eecfac2027d74f55c1046d7fa33d836d5f7f9839
ezarnosky <ezarnosky@gmail.com>
2015-06-21 17:26:28 -0700

    Friggin Typos

--- bb-installer-stub.sh
+++ bb-installer-stub.sh
@@ -4,13 +4,20 @@
 #--------> Prepare environment
 
 #----> Build folder structure
+echo "Build folder structure"
 mkdir -p /bandito-box/apps                   # Default install folder for all apps
 
 #----> Install dependencies
-apt-get update && apt-get upgrade -y && apt-transport-https git-core -y && apt-get autoremove -y
+echo "Install dependencies"
+apt-get update && apt-get upgrade -y && apt-get install apt-transport-https git-core -y && apt-get autoremove -y
 
 #----> Clone Git Repository
+echo "Clone Git Repository"
 git clone https://github.com/EZarnosky/Bandito /bandito-box/apps/Bandito-Box
 
 #----> Start the main installer
-chmod +x /bandito-box/apps/Bandito-Box/bb-installer.sh
+echo "Start the main installer"
+chmod +x /bandito-box/apps/Bandito-Box/bb-installer.sh && /bandito-box/apps/Bandito-Box/bb-installer.sh
+
+#--> Mod module scripts to make executable
+chmod +x /bandito-box/apps/Bandito-Box/modules/*.sh
--- bb-installer.sh
+++ bb-installer.sh
@@ -1,11 +1,11 @@
 #----------------> Bandito Box Installer
-#
+echo "Bandito Box Installer"
+
 #--------> Prepare environment
-#
-#---->
-apt-get update && apt-get upgrade -y && apt-get install zip unzip p7zip-full cifs-utils ntfs-3g build-essential make gcc python python-dev python-pip -y && apt-get autoremove -y
+echo "Prepare environment"
 
 #----> Build folder structure
+echo "Build folder structure"
 mkdir -p /bandito-box/.packages              # Download location for zip and deb files to 
 mkdir -p /bandito-box/logs                   # Log file location for all apps
 mkdir -p /bandito-box/.conf/ssl              # SSL Certs folder
@@ -17,10 +17,13 @@ mkdir -p /bandito-box/.backup                # Location for applicaiton backups 
 mkdir -p /bandito-box/media                  # Media location - typically a SMB share
 
 #----> Install dependencies
+echo "Install dependencies"
 apt-get update && apt-get upgrade -y && apt-get install zip unzip p7zip-full cifs-utils ntfs-3g build-essential make gcc -y && apt-get autoremove -y
 
 #----> Mount drive (USB & Network)
+echo "Mount drive (USB & Network)"
 #--> Update fstab
+echo "Update fstab"
 echo '' >> /etc/fstab
 echo '# USB Download storage' >> /etc/fstab
 echo 'UUID=USB_DRIVE_UUID /bandito-box/.data/USB ntfs-3g uid=1000,gid=1000,umask=007 0 0' >> /etc/fstab
@@ -31,16 +34,18 @@ echo "//x.x.x.x/Backup/Applications/Bandito-Box /bandito-box/.backup cifs userna
 echo '' >> /etc/fstab
 
 #--> Perform mount of all drives in fstab
+echo "Perform mount of all drives in fstab"
 mount -a
 
-#----> Change MOTD
+#----> Change MOTD banner
+echo "Change MOTD banner"
 mv /etc/motd /bandito-box/.backup/motd.backup
 tr -d '\r' < /bandito-box/apps/Bandito-Box/conf/etc_motd > /etc/motd
 
 #----> Add records to hosts file
+echo "Add records to hosts file"
 cp /etc/hosts /bandito-box/.backup/hosts.backup
 tr -d '\r' < /bandito-box/apps/Bandito-Box/conf/etc_hosts > /bandito-box/tmp/etc_hosts && cat /bandito-box/tmp/etc_hosts >> /etc/hosts
 rm /bandito-box/tmp/etc_hosts
 
-
-
+echo "Time to install individual apps... WIP"
